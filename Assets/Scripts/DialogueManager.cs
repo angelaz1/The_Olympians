4c71c301e2e30f2ce7aded41e1c223fea5ca50ed
Angela Zhang <angelaz1@andrew.cmu.edu>
2020-04-09 14:57:08 -0500

    Basic ui setup for dialogue

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class DialogueManager : MonoBehaviour
 {
     public static DialogueManager dm;
+
     // We will use the speakerName to find and parse the dialogue JSON
     // The dialogue for all characters should be in a JSON file named their name
     public string speakerName;
@@ -26,14 +27,18 @@ public class DialogueManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        string json = File.ReadAllText(Application.dataPath + "/" + speakerName + ".json");
-        dialogue = JsonUtility.FromJson<CharacterDialogue>(json);
-        Debug.Log(dialogue.checkpointDialogue[0].dialogue[0].text);
+        speakerName = "Aphrodite";
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void readDialogue() {
+        string json = File.ReadAllText(Application.dataPath + "/" + speakerName + ".json");
+        dialogue = JsonUtility.FromJson<CharacterDialogue>(json);
+        Debug.Log(dialogue.checkpointDialogue[0].dialogue[0].text);
     }
 }
