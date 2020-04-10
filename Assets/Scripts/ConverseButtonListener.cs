08c215d5f9a0552d55f81e6e29affbf4f8244afd
Angela Zhang <angelaz1@andrew.cmu.edu>
2020-04-09 20:24:52 -0500

    UI fixes woo

---
+++ Assets/Scripts/ConverseButtonListener.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConverseButtonListener : ButtonListener
+{
+    public override void onClick() {
+        GameObject.Find("InteractButtonManager").GetComponent<InteractButtonManager>().allFlyOff();
+        // START DIALOGUE
+    }
+}
--- Assets/Scripts/InteractButtonManager.cs
+++ Assets/Scripts/InteractButtonManager.cs
@@ -1,26 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InteractButtonManager : MonoBehaviour
 {
+    private ButtonListener[] listeners; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        listeners = new ButtonListener[3];
+        listeners[0] = GameObject.Find("ConverseButton").GetComponent<ButtonListener>();
+        listeners[1] = GameObject.Find("PostButton").GetComponent<ButtonListener>();
+        listeners[2] = GameObject.Find("DateButton").GetComponent<ButtonListener>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
-    public void onHover() {
-        this.GetComponent<Animator>().SetBool("isHovering", true);
-    }
-
-    public void offHover() {
-        this.GetComponent<Animator>().SetBool("isHovering", false);
+    public void allFlyOff() {
+        for(int i = 0; i < listeners.Length; i++) {
+            listeners[i].setFlyingOff();
+        }
     }
 }
