cd65c5e953338e73a17aed687f2ed8426574b603
Angela Zhang <angelaz1@andrew.cmu.edu>
2020-04-14 01:49:31 -0500

    Animations for posting UI

--- Assets/Scripts/PostingUIManager.cs
+++ Assets/Scripts/PostingUIManager.cs
@@ -5,12 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class PostingUIManager : MonoBehaviour
 {
-    private GameManager gameManager;
+    // private GameManager gameManager;
 
     // Start is called before the first frame update
     void Start()
     {
-        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        // gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
