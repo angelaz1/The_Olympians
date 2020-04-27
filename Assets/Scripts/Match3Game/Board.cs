87a0049d346d1e6712d0088f409e5c04f547c673
Darien <dweems@andrew.cmu.edu>
2020-04-27 12:48:00 -0400

    Matches destroyed, columns refill

--- Assets/Scripts/Match3Game/Board.cs
+++ Assets/Scripts/Match3Game/Board.cs
@@ -24,6 +24,15 @@ public class Board : MonoBehaviour
             {
                 Vector2 tempPosition = new Vector2(i, j);
                 int orbToUse = Random.Range(0, orbs.Length);
+
+                int maxIterations = 0;
+                while(MatchesAt(i, j, orbs[orbToUse]) && maxIterations < 100)
+                {
+                    orbToUse = Random.Range(0, orbs.Length);
+                    maxIterations++;
+                }
+                maxIterations = 0;
+
                 GameObject orb = Instantiate(orbs[orbToUse], tempPosition, Quaternion.identity);
                 orb.transform.parent = this.transform;
                 orb.name = "(" + i + ", " + j + ")";
@@ -32,9 +41,129 @@ public class Board : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    private bool MatchesAt(int col, int row, GameObject orb)
     {
-        
+        if(col > 1 && row > 1)
+        {
+            if(allOrbs[col - 1, row].tag == orb.tag && allOrbs[col - 2, row].tag == orb.tag)
+            {
+                return true;
+            }
+            if(allOrbs[col, row - 1].tag == orb.tag && allOrbs[col, row - 2].tag == orb.tag)
+            {
+                return true;
+            }
+        }
+        else if(col <= 1 || row <= 1)
+        {
+            if(row > 1)
+            {
+                if(allOrbs[col, row - 1].tag == orb.tag && allOrbs[col, row - 2].tag == orb.tag)
+                {
+                    return true;
+                }
+            }
+            if(col > 1)
+            {
+                if(allOrbs[col - 1, row].tag == orb.tag && allOrbs[col - 2, row].tag == orb.tag)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private void DestroyMatchesAt(int col, int row)
+    {
+        if(allOrbs[col, row].GetComponent<Orb>().isMatched)
+        {
+            Destroy(allOrbs[col, row]);
+            allOrbs[col, row] = null;
+        }
+    }
+
+    public void DestroyMatches()
+    {
+        for(int i = 0; i < xSize; i++)
+        {
+            for(int j = 0; j < ySize; j++)
+            {
+                if(allOrbs[i, j] != null)
+                {
+                    DestroyMatchesAt(i, j);
+                }
+            }
+        }
+        StartCoroutine(DecreaseRowCo());
+    }
+
+    private IEnumerator DecreaseRowCo()
+    {
+        int nullCount = 0;
+        for(int i = 0; i < xSize; i++)
+        {
+            for(int j = 0; j < ySize; j++)
+            {
+                if(allOrbs[i, j] == null)
+                {
+                    nullCount++;
+                }
+                else if(nullCount > 0)
+                {
+                    allOrbs[i, j].GetComponent<Orb>().row -= nullCount;
+                    allOrbs[i, j] = null;
+                }
+            }
+            nullCount = 0;
+        }
+        yield return new WaitForSeconds(0.4f);
+        StartCoroutine(FillBoardCo());
+    }
+
+    private void RefillBoard()
+    {
+        for(int i = 0; i < xSize; i++)
+        {
+            for(int j = 0; j < ySize; j++)
+            {
+                if(allOrbs[i, j] == null)
+                {
+                    Vector2 tempPosition = new Vector2(i, j);
+                    int orbToUse = Random.Range(0, orbs.Length);
+                    GameObject orb = Instantiate(orbs[orbToUse], tempPosition, Quaternion.identity);
+                    allOrbs[i, j] = orb;
+                }
+            }
+        }
+    }
+
+    private bool MatchesOnBoard()
+    {
+        for(int i = 0; i < xSize; i++)
+        {
+            for(int j = 0; j < ySize; j++)
+            {
+                if(allOrbs[i, j] != null)
+                {
+                    if(allOrbs[i, j].GetComponent<Orb>().isMatched)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private IEnumerator FillBoardCo()
+    {
+        RefillBoard();
+        yield return new WaitForSeconds(0.5f);
+        while(MatchesOnBoard())
+        {
+            yield return new WaitForSeconds(0.5f);
+            DestroyMatches();
+        }
     }
 }
--- Assets/Scripts/Match3Game/Orb.cs
+++ Assets/Scripts/Match3Game/Orb.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Orb : MonoBehaviour
 {
     public int col, row;
-    public int targetX, targetY;
+    private int targetX, targetY;
+    public bool isMatched = false;
     private Board board;
     private GameObject otherOrb;
     private Vector2 firstTouchPos, finalTouchPos;
     private Vector2 tempPosition;
-    public float swipeAngle = 0;
+    private float swipeAngle = 0.0f;
+    private float swipeResist = 1.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -25,6 +27,13 @@ public class Orb : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        FindMatches();
+        if(isMatched)
+        {
+            SpriteRenderer orbSprite = GetComponent<SpriteRenderer>();
+            orbSprite.color = new Color(0.0f, 0.0f, 0.0f, 0.2f);
+        }
+
         targetX = col;
         targetY = row;
         // Horizontal Movement
@@ -32,12 +41,15 @@ public class Orb : MonoBehaviour
         {
             tempPosition = new Vector2(targetX, transform.position.y);
             transform.position = Vector2.Lerp(transform.position, tempPosition, 0.4f);
+            if(board.allOrbs[col, row] != this.gameObject)
+            {
+                board.allOrbs[col, row] = this.gameObject;
+            }
         }
         else
         {
             tempPosition = new Vector2(targetX, transform.position.y);
             transform.position = tempPosition;
-            board.allOrbs[col, row] = this.gameObject;
         }
 
         // Vertical Movement
@@ -45,12 +57,15 @@ public class Orb : MonoBehaviour
         {
             tempPosition = new Vector2(transform.position.x, targetY);
             transform.position = Vector2.Lerp(transform.position, tempPosition, 0.4f);
+            if(board.allOrbs[col, row] != this.gameObject)
+            {
+                board.allOrbs[col, row] = this.gameObject;
+            }
         }
         else
         {
             tempPosition = new Vector2(transform.position.x, targetY);
             transform.position = tempPosition;
-            board.allOrbs[col, row] = this.gameObject;
         }
     }
 
@@ -67,20 +82,23 @@ public class Orb : MonoBehaviour
 
     void CalculateAngle()
     {
-        swipeAngle = Mathf.Atan2(finalTouchPos.y - firstTouchPos.y, finalTouchPos.x - firstTouchPos.x) * 180 / Mathf.PI;
-        MoveOrbs();
+        if(Mathf.Abs(finalTouchPos.y - firstTouchPos.y) > swipeResist || Mathf.Abs(finalTouchPos.x - firstTouchPos.x) > swipeResist)
+        {
+            swipeAngle = Mathf.Atan2(finalTouchPos.y - firstTouchPos.y, finalTouchPos.x - firstTouchPos.x) * 180 / Mathf.PI;
+            MoveOrbs();
+        }
     }
 
     void MoveOrbs()
     {
-        if(swipeAngle > -45 && swipeAngle <= 45 && col < board.xSize)
+        if(swipeAngle > -45 && swipeAngle <= 45 && col < board.xSize - 1)
         {
             // Right Swipe
             otherOrb = board.allOrbs[col + 1, row];
             otherOrb.GetComponent<Orb>().col -= 1;
             col += 1;
         }
-        else if(swipeAngle > 45 && swipeAngle <= 135 && row < board.ySize)
+        else if(swipeAngle > 45 && swipeAngle <= 135 && row < board.ySize - 1)
         {
             // Up Swipe
             otherOrb = board.allOrbs[col, row + 1];
@@ -94,12 +112,56 @@ public class Orb : MonoBehaviour
             otherOrb.GetComponent<Orb>().col += 1;
             col -= 1;
         }
-        else if((swipeAngle > -45 || swipeAngle <= 45) && row > 0)
+        else if(swipeAngle < -45 && swipeAngle >= -135 && row > 0)
         {
             // Down Swipe
             otherOrb = board.allOrbs[col, row - 1];
             otherOrb.GetComponent<Orb>().row += 1;
             row -= 1;
+        }
+        StartCoroutine(ClearMatches());
+    }
+
+    void FindMatches()
+    {
+        if(col > 0 && col < board.xSize - 1)
+        {
+            GameObject leftOrb1 = board.allOrbs[col - 1, row];
+            GameObject rightOrb1 = board.allOrbs[col + 1, row];
+            if(leftOrb1 != null && rightOrb1 != null)
+            {
+                if (leftOrb1.tag == this.gameObject.tag && rightOrb1.tag == this.gameObject.tag)
+                {
+                    leftOrb1.GetComponent<Orb>().isMatched = true;
+                    rightOrb1.GetComponent<Orb>().isMatched = true;
+                    isMatched = true;
+                }
+            }
+        }
+
+        if(row > 0 && row < board.ySize - 1)
+        {
+            GameObject upOrb1 = board.allOrbs[col, row + 1];
+            GameObject downOrb1 = board.allOrbs[col, row - 1];
+            if(upOrb1 != null && downOrb1 != null)
+            {
+                if (upOrb1.tag == this.gameObject.tag && downOrb1.tag == this.gameObject.tag)
+                {
+                    upOrb1.GetComponent<Orb>().isMatched = true;
+                    downOrb1.GetComponent<Orb>().isMatched = true;
+                    isMatched = true;
+                }
+            }
+        }
+    }
+
+    public IEnumerator ClearMatches()
+    {
+        yield return new WaitForSeconds(0.5f);
+        if(otherOrb != null)
+        {
+            board.DestroyMatches();
+            otherOrb = null;
         }
     }
 }
