6272d7bd624d715f3eec280408cff571865281f5
Angela Zhang <angelaz1@andrew.cmu.edu>
2020-04-27 12:36:37 -0500

    tutorial building animations

---
+++ Assets/Scripts/TutorialManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    private GameObject canvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas = GameObject.Find("Canvas");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void clickedNotif() {
+        canvas.GetComponent<Animator>().SetTrigger("ClickedNotif");
+    }
+}
